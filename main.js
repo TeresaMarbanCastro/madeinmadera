ce93434904ba8382d18f0580d0147f5ba9cba5bd
Teresa <teresamarbancastro@gmail.com>
2018-07-05 17:35:50 +0200

    pannels added

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-'use strict'
+
+  'use strict';
+function application(){
+    const panels = document.querySelectorAll('.panel');
+    function toggleOpen(){
+        this.classList.toggle('open');
+}
+
+function toggleActive(e){
+    // console.lob(e.propertyName);
+    if(e.propertyName.includes('flex')){
+        this.classList.toggle('open-active');
+    }
+}
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+}
